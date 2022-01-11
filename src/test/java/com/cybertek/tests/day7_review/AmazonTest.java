ead4d67c25e3b8e54a14e99e25e757ea79f91cfc
Javid146 <mammadli.javid15@gmail.com>
2022-01-10 21:12:13 -0500

    review

---
+++ src/test/java/com/cybertek/tests/day7_review/AmazonTest.java
@@ -0,0 +1,22 @@
+package com.cybertek.tests.day7_review;
+
+public class AmazonTest {
+    public static void main(String[] args) {
+        nameMatchTest();
+
+
+    }
+/*
+* find item from amazon
+* remember name of item
+* click*/
+    public static void nameMatchTest() {
+
+
+
+
+
+
+
+    }
+}
